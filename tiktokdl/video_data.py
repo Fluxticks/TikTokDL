bdc30307ceb67136b1dc4af047ba9aa931ca6778
Fluxticks <benjigarment.appdev@gmail.com>
2023-05-26 13:21:22 +0200

    Added Video dataclass

---
+++ tiktokdl/video_data.py
@@ -0,0 +1,20 @@
+from dataclasses import dataclass
+from datetime import datetime
+
+
+@dataclass()
+class TikTokVideo:
+    url: str
+    video_id: str
+    author_username: str
+    author_display_name: str
+    author_avatar: str
+    author_url: str
+    video_description: str
+    timestamp: datetime
+    like_count: int
+    share_count: int
+    comment_count: int
+    view_count: int
+    video_thumbnail: str
+    file_path: str = None
