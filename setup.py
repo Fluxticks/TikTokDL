f1ffbf802a526502b77509ef6fa998065fa37129
Fluxticks <benjigarment.appdev@gmail.com>
2023-06-13 08:46:24 +0200

    Fixed issues with pip package install

--- setup.py
+++ setup.py
@@ -1,21 +1,19 @@
 from setuptools import setup, find_packages
 
-with open("requirements.txt") as f:
-    requirements = f.read()
-
-with open("README.md") as f:
-    readme = f.read()
-
 setup(
     name="tiktok-dlpy",
-    version="1.1.0",
+    version="1.1.1",
     url="https://github.com/Fluxticks/TikTokDL",
-    download_url="https://github.com/Fluxticks/TikTokDL/archive/v1.1.0.tar.gz",
+    download_url="https://github.com/Fluxticks/TikTokDL/archive/v1.1.1.tar.gz",
     author="Fluxticks",
     packages=find_packages(),
-    install_requires=requirements,
-    long_description=readme,
-    long_description_content_type="text/markdown",
+    install_requires=[
+        "bs4",
+        "lxml",
+        "playwright",
+        "numpy",
+        "opencv-python",
+    ],
     description="A package to dowload TikTok videos via URL",
     classifiers=[
         "Development Status :: 5 - Production/Stable",
