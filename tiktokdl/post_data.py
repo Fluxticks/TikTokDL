f19de79589a994b628fdafe02e9d09aca3c9dbb8
Fluxticks <benjigarment.appdev@gmail.com>
2023-07-31 14:41:13 +0200

    Created TikTokPost classes and subclasses

--- tiktokdl/post_data.py
+++ tiktokdl/post_data.py
@@ -3,19 +3,28 @@ from datetime import datetime
 
 
 @dataclass()
-class TikTokVideo:
+class TikTokPost:
     url: str
-    video_id: str
+    post_id: str
     author_username: str
     author_display_name: str
     author_avatar: str
     author_url: str
-    video_download_setting: int
-    video_description: str
+    post_download_setting: int
+    post_description: str
     timestamp: datetime
     like_count: int
     share_count: int
     comment_count: int
     view_count: int
+
+
+@dataclass()
+class TikTokVideo(TikTokPost):
     video_thumbnail: str
     file_path: str = None
+
+
+@dataclass()
+class TikTokSlide(TikTokPost):
+    images: list[str] = None
