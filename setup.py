a52fdb600482ff51de517ef0af8e1811b613c703
Fluxticks <benjigarment.appdev@gmail.com>
2023-08-06 00:34:22 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup, find_packages
 
 setup(
     name="tiktok-dlpy",
-    version="1.3.1",
+    version="1.4.0",
     url="https://github.com/Fluxticks/TikTokDL",
-    download_url="https://github.com/Fluxticks/TikTokDL/archive/v1.3.1.tar.gz",
+    download_url="https://github.com/Fluxticks/TikTokDL/archive/v1.4.0.tar.gz",
     author="Fluxticks",
     packages=find_packages(),
     install_requires=[
