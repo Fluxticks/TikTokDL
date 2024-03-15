f02938b92bf3b9d1d1ff7bd452fbc58135664ef4
Fluxticks <dev.garmentbecky@gmail.com>
2024-03-15 22:09:36 +0100

    Update test URLs

--- tests/test_urls.py
+++ tests/test_urls.py
@@ -10,9 +10,9 @@ class Test_TestTikTokURL(unittest.TestCase):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.url_1 = "https://vm.tiktok.com/ZGeYy3Ekf/"
-        self.url_2 = "https://vm.tiktok.com/ZGeY1YeXk/"
-        self.url_3 = "https://vm.tiktok.com/ZGeYA9CJ4/"  # slideshow
+        self.url_1 = "https://vm.tiktok.com/ZGeUuafAg/"
+        self.url_2 = "https://vm.tiktok.com/ZGeUuafAg/"
+        self.url_3 = "https://vm.tiktok.com/ZGefwPvNt/"  # slideshow
 
     def assert_data(self, expected_data, actual_data):
         self.assertIsNotNone(actual_data)
@@ -23,23 +23,22 @@ class Test_TestTikTokURL(unittest.TestCase):
 
     def test_url_1(self):
         expected_data = TikTokVideo(
-            url="https://tiktok.com/@jimdoga/video/7302355630109773057",
-            post_id="7302355630109773057",
-            author_username="jimdoga",
-            author_display_name="Jimdoga",
-            author_avatar="some-url",
-            author_url="https://tiktok.com/@jimdoga",
+            url='https://tiktok.com/@ben_reilly_/video/7341431726746438944',
+            post_id='7341431726746438944',
+            author_username='ben_reilly_',
+            author_display_name='OurGloriousGermany',
+            author_avatar='some-url',
+            author_url='https://tiktok.com/@ben_reilly_',
             post_download_setting=0,
-            post_description="never joining an smp late again... #minecraft "
-            "#minecraftmoment #fyp #mcyt #herobrine #foryou "
-            "#foryoupage",
-            timestamp=datetime(2023, 11, 17, 10, 9, 26),
-            like_count=205900,
-            share_count=1572,
-            comment_count=778,
-            view_count=1900000,
-            video_thumbnail="some-url",
-            file_path=None)
+            post_description='Watch till the end Frankfurt is unreal😍🇩🇪 Please '
+            'comment which places you want next! #travel '
+            '#frankfurt #world ',
+            timestamp=datetime(2024, 3, 1, 17, 24, 40),
+            like_count=281600,
+            share_count=8943,
+            comment_count=5857,
+            view_count=2400000,
+            video_thumbnail="some-url")
 
         actual_data = asyncio.run(get_post(url=self.url_1, download=False))
 
@@ -47,22 +46,22 @@ class Test_TestTikTokURL(unittest.TestCase):
 
     def test_url_2(self):
         expected_data = TikTokVideo(
-            url="https://tiktok.com/@dafuqboom_94/video/7307437770128067871",
-            post_id="7307437770128067871",
-            author_username="dafuqboom_94",
-            author_display_name="Dafuq!?Boom!",
-            author_avatar="some-url",
-            author_url="https://tiktok.com/@dafuqboom_94",
+            url='https://tiktok.com/@ben_reilly_/video/7341431726746438944',
+            post_id='7341431726746438944',
+            author_username='ben_reilly_',
+            author_display_name='OurGloriousGermany',
+            author_avatar='some-url',
+            author_url='https://tiktok.com/@ben_reilly_',
             post_download_setting=0,
-            post_description="The End 🤣🤣 #funny #funnyvideos #animals #dog "
-            "#cat #pet #viarl #foryou #fyp (540)",
-            timestamp=datetime(2023, 12, 1, 2, 50, 51),
-            like_count=211500,
-            share_count=8454,
-            comment_count=2016,
-            view_count=3500000,
-            video_thumbnail="some-url",
-            file_path=None)
+            post_description='Watch till the end Frankfurt is unreal😍🇩🇪 Please '
+            'comment which places you want next! #travel '
+            '#frankfurt #world ',
+            timestamp=datetime(2024, 3, 1, 17, 24, 40),
+            like_count=281600,
+            share_count=8943,
+            comment_count=5857,
+            view_count=2400000,
+            video_thumbnail="some-url")
 
         actual_data = asyncio.run(get_post(url=self.url_2, download=False))
 
@@ -70,21 +69,23 @@ class Test_TestTikTokURL(unittest.TestCase):
 
     def test_url_3(self):
         expected_data = TikTokSlide(
-            url=
-            "https://tiktok.com/@the.finals.content/video/7316903402024324384",
-            post_id="7316903402024324384",
-            author_username="the.finals.content",
-            author_display_name="TheFinals Content",
-            author_avatar="some-url",
-            author_url="https://tiktok.com/@the.finals.content",
+            url='https://tiktok.com/@stimuli_adhd/video/7306931770056772896',
+            post_id='7306931770056772896',
+            author_username='stimuli_adhd',
+            author_display_name='Stimuli ADHD',
+            author_avatar='some-url',
+            author_url='https://tiktok.com/@stimuli_adhd',
             post_download_setting=0,
-            post_description="... #thefinals #thefinalsgame "
-            "#thefinalsgameplay ",
-            timestamp=datetime(2023, 12, 26, 15, 2, 17),
-            like_count=411500,
-            share_count=15000,
-            comment_count=6382,
-            view_count=4400000,
+            post_description='milly the intern is self aware to the point of '
+            'upsetting herself x (tag yourself im the '
+            'dismissive doctor)  #adhd #wrapped '
+            '#spotifywrapped #memetherapy #swiftie '
+            '#newlydiagnosedadhd @Spotify @spotifyuk ',
+            timestamp=datetime(2023, 11, 29, 18, 7, 13),
+            like_count=60600,
+            share_count=3381,
+            comment_count=305,
+            view_count=505500,
             images=[])
 
         actual_data = asyncio.run(get_post(url=self.url_3, download=False))
