c19e87601c1478f46476ce679c17df04c0d993af
Fluxticks <dev.garmentbecky@gmail.com>
2024-03-15 21:47:07 +0100

    Implement RetryLimitReached custom exception

--- tiktokdl/exceptions.py
+++ tiktokdl/exceptions.py
@@ -4,6 +4,9 @@ class TikTokBaseException(Exception):
         super().__init__(*args)
         self.url = url
 
+    def __str__(self):
+        return f"TikTok exception occured when accessing {self.url}"
+
 
 class CaptchaFailedException(TikTokBaseException):
 
@@ -21,3 +24,15 @@ class ResponseParseException(TikTokBaseException):
 
     def __init__(self, url: str, *args: any):
         super().__init__(url, *args)
+
+
+class RetryLimitReached(Exception):
+
+    def __init__(self, error: Exception, retries: int, url: str, *args: any):
+        super().__init__(*args)
+        self.offending_error = error
+        self.max_retries = retries
+        self.url = url
+
+    def __str__(self):
+        return f"The maximum number of retries was reached ({self.max_retries}). The last error that occured was:\n\n {self.offending_error.__class__.__name__}: {self.offending_error!s}"
