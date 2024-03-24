84eab7294b3fb4c5c248279ebc7d374b2049d9ca
Fluxticks <dev.garmentbecky@gmail.com>
2024-03-24 15:35:57 +0100

    Bump version to v2.0.0

--- setup.py
+++ setup.py
@@ -7,9 +7,9 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="tiktok-dlpy",
-    version="1.6.0",
+    version="2.0.0",
     url="https://github.com/Fluxticks/TikTokDL",
-    download_url="https://github.com/Fluxticks/TikTokDL/archive/v1.6.0.tar.gz",
+    download_url="https://github.com/Fluxticks/TikTokDL/archive/v2.0.0.tar.gz",
     author="Fluxticks",
     packages=find_packages(),
     install_requires=[
@@ -18,16 +18,15 @@ setup(
         "opencv-python",
     ],
     long_description=long_description,
-    long_description_content_type='text/markdown',
-    description=
-    "A package to download TikTok videos or slideshows by URL without needing to login",
+    long_description_content_type="text/markdown",
+    description="A package to download TikTok videos or slideshows by URL without needing to login",
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10"
+        "Programming Language :: Python :: 3.10",
     ],
     keywords=["tiktok", "playwright", "async"],
 )
